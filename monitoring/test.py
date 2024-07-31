db08aa7d7a170c944e62f23700d0fba6d3ffd032
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-07-31 14:07:37 -0400

    Added swarm connection addresses

--- monitoring/test.py
+++ monitoring/test.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 from ipldstore import get_ipfs_mapper
 import xarray as xr
 from multiformats import CID
@@ -7,6 +8,10 @@ if len(sys.argv) != 2:
     script_name = sys.argv[0]
     print(f"Usage: python {script_name} <zarr_cid>")
     sys.exit(1)
+
+# Make sure we are connect to the servers
+print("Ensuring ipfs swarm connections...")
+subprocess.run(["sh", "swarm-connect.sh"], check=True)
 
 zarr_cid = sys.argv[1]
 
