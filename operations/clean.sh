dffddd0d7be30de1e9c896a5a30ba6d50f9c175f
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-08-02 17:53:44 -0400

    Move clean script

--- operations/clean.sh
+++ operations/clean.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+# Immediately exit on error
+set -e
+
+original_dir=$(pwd)
+
+# Change to the directory of the script
+script_dir=$(dirname "$0")
+cd "$script_dir"
+
+# Switch to the repo directory since this script is inside of the operations directory
+cd ..
+
 for arg in "$@"; do
     case "$arg" in
         nc)
@@ -16,6 +28,10 @@ for arg in "$@"; do
             ;;
         *)
             echo "Unknown argument: $arg" >&2
+            echo "See script switch case statements for all options"
             ;;
     esac
 done
+
+# Return to the original directory
+cd "$original_dir"
