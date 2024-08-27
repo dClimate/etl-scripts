5740f856ba9350cca16bf5de529467ae56d4079d
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-08-27 16:36:11 -0400

    Add auto extraction and stop at >2024-02-01 error dates

--- prism/download.sh
+++ prism/download.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-
 # Function to handle script interruption
 cleanup() {
     echo -e "\nPRISM download script interrupted. Cleaning up and exiting..."
@@ -18,9 +17,6 @@ generate_available() {
 
     cd "$dataset_dir" # cd to the dataset's directory
 
-    # Clear file
-    truncate -s 0 "$available_for_download_txt"
-
     echo Finding the years of available data for "$dataset_dir"
     local years_list_url="https://ftp.prism.oregonstate.edu/daily/$data_type/"
     local years_list=$(curl --silent "$years_list_url" | grep --extended-regexp '[1-2][0-9]{3}\/' | sed -E 's/.*>([1-2][0-9]{3})\/<.*/\1/' | sort)
@@ -28,33 +24,20 @@ generate_available() {
     local latest_year=$(echo "$years_list" | tail -n 1)
 
     echo Retrieving dates available for data from $earliest_year to $latest_year
-    local date_list_per_year_urls=$(echo "$years_list" | awk "{print \"$years_list_url\" \$1 \"/\"}")
-    local date_list_results=$(echo "$date_list_per_year_urls" | xargs --max-procs=0 -n 1 curl --silent)
+    local earliest_year_dates_url="https://ftp.prism.oregonstate.edu/daily/$data_type/$earliest_year/"
+    local latest_year_dates_url="https://ftp.prism.oregonstate.edu/daily/$data_type/$latest_year/"
+    # Matches all bil files in the ftp directory listing
+    local prism_file_grep_regexp='>PRISM_(ppt|tmax|tmin)_.*[0-9]{8}_bil\.zip<'
+    # find all YYYYMMDD strings
+    local yyyymmdd_sed_regexp='s/.*([1-2][0-9]{3}[0-1][0-9][0-3][0-9]).*/\1/'
 
-    local range_query_urls=""
+    # grep -o prints each result match in a newline
+    local earliest_date=$(curl --silent "$earliest_year_dates_url" | grep -oE "$prism_file_grep_regexp" | sed -E "$yyyymmdd_sed_regexp" | sort | head -n 1)
+    local latest_date=$(curl --silent "$latest_year_dates_url" | grep -oE "$prism_file_grep_regexp" | sed -E "$yyyymmdd_sed_regexp" | sort | tail -n 1)
 
-    local year=$earliest_year
-    while [ $year -le $latest_year ]; do
-        # Get the earliest and latest date available for this year
-        local dates_list_html=$(echo "$date_list_results" | grep -E "$year[0-1][0-9][0-3][0-9]_bil\.zip")
-        local dates_list=$(echo "$dates_list_html" | grep -E 'PRISM_(ppt|tmax|tmin)_.*[0-9]{8}_bil\.zip' | sed -E 's/.*([1-2][0-9]{3}[0-1][0-9][0-3][0-9])_bil\.zip<.*/\1/' | sort)
-        local earliest_date=$(echo "$dates_list" | head -n 1)
-        local latest_date=$(echo "$dates_list" | tail -n 1)
-
-        # Setup the range request with these dates
-        local new_url="https://services.nacse.org/prism/data/public/releaseDate/$data_type/$earliest_date/$latest_date"
-        # Concatenate with a newline
-        range_query_urls="$range_query_urls
-$new_url"
-
-        # Move to the next year
-        year=$((year + 1))
-    done
-
-    echo Constructing list of all dates with grid counts and download links...
-    local all_ranges=$(echo "$range_query_urls" | xargs --max-procs=0 -n 1 curl --silent)
-    echo Writing list to "$dataset_dir/$available_for_download_txt"
-    echo "$all_ranges" | sort | awk '{print $1, $4, $5 "?format=nc"}' > "$available_for_download_txt"
+    echo Writing list of all dates from $earliest_date to $latest_date to "$dataset_dir/$available_for_download_txt"
+    local range_query_url="https://services.nacse.org/prism/data/public/releaseDate/$data_type/$earliest_date/$latest_date"
+    curl --silent $range_query_url | awk '{print $1 " " $4 " " $5 "?format=nc"}' > "$available_for_download_txt"
 
     cd ..
 }
@@ -63,36 +46,46 @@ download() {
     local dataset_dir="$1"
     local available_for_download_txt="available-for-download.txt"
 
-    # For every possible date, check if we already have it, and if not, download it
-    # to_be_downloaded=$(cat "$dataset_dir/$available_for_download_txt" | xargs --max-procs=0 -n 1 bash update_date.sh "$dataset_dir")
-    # echo $to_be_downloaded
+    cd $dataset_dir
 
-    cd "$dataset_dir"
-
-    while read date_info; do
-        data_date=$(echo "$date_info" | awk '{print $1}')
-        upstream_grid_count=$(echo "$date_info" | awk '{print $2}')
-        download_url=$(echo "$date_info" | awk '{print $3}')
-
-        # The first portion,  ls -p | grep -v / , gets only the files
-        # grep -E 'regexp' just finds the files in the format YYYY-MM-DD_N.nc.zip
-        # sed then trasnforms that into a list YYYY-MM-DD N
-        current_download_info=$(ls -p | grep -v / | grep -E "${data_date}_[0-8]\.nc" | sed -E 's/([1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9])_([1-8])\.nc/\1 \2/')
-        current_grid_count=$(echo "$current_download_info" | awk '{print $2}')
-
+    # Check, for every possible date, if we have the most up to date netCDF file
+    # If not, download and extract it from the zip
+    while read data_date upstream_grid_count raw_dl_url; do
         nc_name="${data_date}_${upstream_grid_count}.nc"
-        zip_name="$nc_name.zip"
-        # If the file does not exist OR the upstream grid count is greater, then download and extract the nc file to this directory
-        if [ -z "$current_download_info" ] || [ $upstream_grid_count -gt $current_grid_count ]; then
+
+        # Temporary stop until PRISM resolves issue with their backend
+        # Currently dates for 2024-02-01 do have errors
+        if [[ $data_date == 2024-02-01 ]]; then
+            echo "Date 2024-02-01 found in data_date. Exiting early."
+            cd ..
+            return
+        fi
+
+        # ! -e = Condition is true if file does not exist
+        if [ ! -e $nc_name ]; then
+            # Remove any older files with an older grid count if needed
+            # We first check if the file exists, otherwise rm will return an error which will exit due to set -e
+            for grid_count in {1..8}
+            do
+                older_nc_name=${data_date}_${grid_count}.nc
+                if [[ -e $rm_name ]]; then
+                    rm $older_nc_name
+                fi
+            done
+
             # Download and overwrite
-            wget -O "$zip_name" "$download_url"
+            zip_name="${nc_name}.zip"
+            wget -O "$zip_name" "$raw_dl_url"
 
             # Unzip and extract just the nc file
-            mkdir -p "$data_date"
-            unzip "$zip_name" -d "$data_date"
-            nc_inside_zip_name=$(ls "$data_date" | grep -E '.*.nc$')
-            mv "$data_date/$nc_inside_zip_name" "./$nc_name"
-            rm -r "$data_date"
+            mkdir -p $data_date
+            unzip $zip_name -d $data_date > /dev/null
+            cd $data_date
+            mv *.nc "${data_date}_${upstream_grid_count}.nc"
+            mv *.nc ../
+            cd ..
+            rm -r $data_date
+            rm $zip_name
 
             # Wait 2 seconds between each download, to avoid overwhelming the PRISM servers
             # 2 seconds is the recommended amount found in PRISM's guide to their web service
