db4653427fb4d26767060cec1b15ef8416444805
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-08-29 15:52:24 -0400

    Add detection of download links file

--- cpc/fetch.sh
+++ cpc/fetch.sh
@@ -27,9 +27,14 @@ esac
 dataset=$1
 
 cd $dataset
-echo "Fetching all .nc files for: $dataset"
+if [[ ! -e download-links.txt ]]; then
+    echo "File $dataset/download-links.txt does not exist!"
+    echo "Please run prefetch.sh for $dataset"
+    exit 1
+fi
 # Using xargs to run as many concurrent downloads as possible
 # --max-procs=0 flag tells it to run as many processes as possible
 # wget's flag --timestamping makes it check to see if files from the incoming server are marked as newer or not than the ones we've already downloaded
 # This makes fetching idempotent, as long as the download files all remain inside the same folder and as the same names
+echo "Fetching all .nc files for: $dataset"
 cat download-links.txt | xargs --max-procs=0 -n 1 wget --timestamping
