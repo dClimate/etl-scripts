20ccafded6994d652f1a4ed72a67d144ce871ed8
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-08-30 16:25:52 -0400

    More efficient listing of files using just globbing

--- cpc/extract.sh
+++ cpc/extract.sh
@@ -8,5 +8,5 @@ check_argument_is_valid $1
 
 dataset=$1
 echo Processing all .nc files for $dataset
-# The weird ls is so that we get absolute paths to send to the netcdf extractor
-ls -d "$PWD/$dataset"/*.nc | xargs -n 1 python ../shared_python/extract_netcdf.py
+# This sends, one by one, absolute paths to the netCDF files to the extractor script
+echo "$PWD/$dataset"/*.nc | xargs -n 1 python ../shared_python/extract_netcdf.py
