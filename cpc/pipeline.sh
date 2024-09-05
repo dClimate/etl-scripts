301348d2fa3cdeb0379cbbcf6dbd0e4dd6e61bd6
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-09-05 17:47:58 -0400

    Simplify pipeline scripts

--- cpc/pipeline.sh
+++ cpc/pipeline.sh
@@ -1,49 +1,20 @@
 #!/bin/bash
 set -e
 
-# Use separate print usage and argument validation functions in pipeline.sh compared to the other shell scripts to allow for the "all" option
-print_usage() {
-    echo Usage: bash "$0" "precip-conus|precip-global|tmin|tmax|all"
-    echo Example: bash "$0" all
-}
-
-num_arguments=$#
-
-# Checks if number of arguments is exactly 1
-if (( num_arguments != 1 )); then
-    echo "Error: Too many arguments"
-    print_usage
-    exit 1
-fi
-
 script_dir=$(dirname "$0")
 cd "$script_dir"
 
-pipeline() {
-    dataset=$1
+source shared_functions.sh
 
-    source ../.venv/bin/activate
+check_there_is_one_argument $#
 
-    bash prefetch.sh $dataset
-    bash fetch.sh $dataset
-    bash transform.sh $dataset
-    bash load_to_ipld.sh $dataset
-    bash update_ipns.sh $dataset
-}
+dataset=$1
+check_argument_is_valid $dataset
 
-case $1 in
-    precip-conus|precip-global|tmax|tmin)
-        pipeline $1
-        ;;
-    all)
-        pipeline precip-conus
-        pipeline precip-global
-        pipeline tmax
-        pipeline tmin
-        ;;
-    *)
-        echo "Error: Unknown argument $arg" >&2
-        print_usage
-        exit 1
-        ;;
-esac
+source ../.venv/bin/activate
+
+bash prefetch.sh $dataset
+bash fetch.sh $dataset
+bash transform.sh $dataset
+bash load_to_ipld.sh $dataset
+bash update_ipns.sh $dataset
