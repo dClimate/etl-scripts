89e9983d80342fe8084c575e01b18c9c8444cb9e
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-09-05 12:39:06 -0400

    Simplify combine python script to take one argument

--- shared_python_scripts/combine.py
+++ shared_python_scripts/combine.py
@@ -4,9 +4,30 @@ This script writes a kerchunk multizarr to the destination folder by combining a
 
 from pathlib import Path
 import re
+import sys
 
 from kerchunk.combine import MultiZarrToZarr
 from msgspec import json
+
+
+def generate_fix_fill_values_preprocessor(fill_value):
+    def fix_fill_values_preprocessor(refs):
+        ref_names = set()
+        file_match_pattern = (
+            r"(.*?)/"  # only modify the Zarr files with a period at the beginning
+        )
+        for ref in refs:
+            match = re.match(file_match_pattern, ref)
+            if match:
+                ref_names.add(match.group(1))
+
+        for ref in ref_names:
+            # Replace the fill value in the zarr metadata
+            zarray_dict = json.decode(refs[f"{ref}/.zarray"])
+            zarray_dict["fill_value"] = fill_value
+            refs[f"{ref}/.zarray"] = json.encode(zarray_dict)
+
+    return fix_fill_values_preprocessor
 
 
 def combine_and_write_multizarr_json(
@@ -17,23 +38,7 @@ def combine_and_write_multizarr_json(
 
     # Only do fill value preprocessing if a fill value is actually specified
     if fill_value is not None:
-
-        def fix_fill_values_preprocessor(refs):
-            ref_names = set()
-            file_match_pattern = (
-                r"(.*?)/"  # only modify the Zarr files with a period at the beginning
-            )
-            for ref in refs:
-                match = re.match(file_match_pattern, ref)
-                if match:
-                    ref_names.add(match.group(1))
-
-            for ref in ref_names:
-                # Replace the fill value in the zarr metadata
-                zarray_dict = json.decode(refs[f"{ref}/.zarray"])
-                zarray_dict["fill_value"] = fill_value
-                refs[f"{ref}/.zarray"] = json.encode(zarray_dict)
-
+        fix_fill_values_preprocessor = generate_fix_fill_values_preprocessor(fill_value)
         multizarr = MultiZarrToZarr(
             json_paths, concat_dims="time", preprocess=fix_fill_values_preprocessor
         )
@@ -48,62 +53,36 @@ def combine_and_write_multizarr_json(
         f.write(f_bytes)
 
 
-if __name__ == "__main__":
-    import sys
+def print_usage():
+    script_call_path = sys.argv[0]
+    print(f"Usage: python {script_call_path} <directory with kerchunk .nc.json files>")
+    print(f"Example: python {script_call_path} chirps/prelim-p25")
 
-    def print_usage():
-        script_call_path = sys.argv[0]
-        print(f"Usage: python {script_call_path} <provider> <dataset>")
-        print(f"Example: python {script_call_path} cpc precip-conus")
 
+def main():
     # Verify the number of arguments
     num_arguments = len(sys.argv) - 1
-    if num_arguments != 2:
-        print(
-            f"Error: Script did not receive only two arguments, was provided {num_arguments} arguments"
-        )
+    if num_arguments != 1:
+        print("Error: Script received more than one argument")
         print_usage()
         sys.exit(1)
 
-    # Verify that the first argument is one of the valid dataset providers
-    data_provider = sys.argv[1]
-    match data_provider:
-        case "cpc":
-            pass
-        case "chirps" | "prism":
-            print(f"Data provider {data_provider} not supported yet")
-            sys.exit(1)
-        case _:
-            print(f"Invalid data provider argument {data_provider}")
-            print_usage()
-            sys.exit(1)
+    # Verify the path is a directory
+    dataset_dir = Path(sys.argv[1])
+    if not dataset_dir.is_dir():
+        print(f"Error: Path {dataset_dir} is not a directory")
+        sys.exit(1)
 
-    # Verify that the second argument is one of the valid datasets in that provider
-    dataset = sys.argv[2]
-    match data_provider:
-        case "cpc":
-            match dataset:
-                case "precip-conus" | "precip-global" | "tmax" | "tmin":
-                    pass
-                case _:
-                    print(f"Invalid dataset {dataset} for provider {data_provider}")
-                    print_usage()
-                    sys.exit(1)
-        case "chirps" | "prism":
-            print(f"Data provider {data_provider} not supported yet")
-            sys.exit(1)
-
-    # Get all the .nc.jsons we need
-    current_file_dir = Path(__file__).parent
-    # The .resolve() removes the ".." from the final path
-    dataset_dir = (current_file_dir / ".." / data_provider / dataset).resolve()
     source_json_paths = list(dataset_dir.glob("*.nc.json"))
     if len(source_json_paths) == 0:
-        print(f"No .nc.json files found in {dataset_dir}")
-        print("Quitting combining")
+        print(f"Error: No .nc.json files found in {dataset_dir}")
         sys.exit(1)
 
     # Call the script functions
     combine_and_write_multizarr_json(
         source_json_paths, dataset_dir, fill_value=-9.96921e36
     )
+
+
+if __name__ == "__main__":
+    main()
