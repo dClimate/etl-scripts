6d96ab2a3202019557a8671e36f3818b5c07db72
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-09-09 17:44:05 -0400

    Add scripts for updating the bootstrap txt files on ipns

--- prism/download-from-bootstrap.sh
+++ prism/download-from-bootstrap.sh
@@ -11,13 +11,31 @@ check_there_is_one_argument $#
 dataset=$1
 check_argument_is_valid $dataset
 
-for year in {1981..2024}; do
-    echo Downloading files for year $year
-    pin_name="bootstrap-prism-${dataset}-${year}.tar.gz"
-    cid=$(ipfs-cluster-ctl pin ls | grep $pin_name | awk '{print $1}')
+cd $dataset
 
-    ipfs get $cid
-    mv $cid $pin_name
-    tar -zxf $pin_name
-    rm $pin_name
-done
+# Based on the dataset, get the ipns hash the list of files are published to
+case $dataset in
+    precip-4km)
+        ipns_hash="/ipns/k51qzi5uqu5dir4p5b2i0pensejrdo9mvr66mxvaq87o0p9t61lyrdul5ws4c6"
+        ;;
+    tmax-4km)
+        ipns_hash="/ipns/k51qzi5uqu5dk5ozhkbrmauboyvckczhopqhaeqsn052wencwlzlrrna38sehg"
+        ;;
+    tmin-4km)
+        ipns_hash="/ipns/k51qzi5uqu5dl2iu0od81lnt2gd838wvbrthjgt129831i00ox98ctg96nsipk"
+        ;;
+esac
+
+echo Getting all .tar.gz files
+cid=$(ipfs name resolve $ipns_hash)
+txt_filename="bootstrap-prism-${dataset}.txt"
+ipfs get $cid -o $txt_filename
+cat $txt_filename | awk '{print $2}' | xargs -n 1 ipfs get
+cat $txt_filename | awk '{print $2 " " $1}' | xargs -n 2 mv
+
+echo Untarring all .tar.gz files to retrieve .nc files
+cat $txt_filename | awk '{print $1}' | xargs -n 1 tar -zxvf
+
+echo Cleaning up by removing .tar.gz files and txt file with list of hashes
+cat $txt_filename | awk '{print $1}' | xargs -n 1 rm
+rm $txt_filename
--- prism/update-bootstrap.sh
+++ prism/update-bootstrap.sh
@@ -13,15 +13,41 @@ check_argument_is_valid $dataset
 
 cd $dataset
 echo Creating bootstrap for $dataset
+
 echo Creating new .tar.gz files for each year
 for year in {1981..2024}; do
-    pin_name="bootstrap-prism-${dataset}-${year}.tar.gz"
     echo $year:tarring and gzipping
     tar -zcf "$year".tar.gz "$year"-??-??_?.nc
-    echo $year: unpinning previous $year.tar.gz if it exists
-    ipfs-cluster-ctl pin ls | grep $pin_name | awk '{print $1}' | xargs -n 1 ipfs-cluster-ctl pin rm
-    echo $year: adding new file
+done
+
+echo Unpinning previous bootstrap .tar.gz files
+ipfs-cluster-ctl pin ls | grep -E "bootstrap-prism-${dataset}-[1-2][0-9]{3}.tar.gz" | awk '{print $1}' | xargs -n 1 ipfs-cluster-ctl pin rm
+
+echo Adding the new .tar.gz files
+for year in {1981..2024}; do
+    echo Adding $year.tar.gz
+
+    pin_name="bootstrap-prism-${dataset}-${year}.tar.gz"
     ipfs-cluster-ctl add --name pin_name $year.tar.gz
-    echo $year: removing $year.tar.gz
+    echo Deleting $year.tar.gz
     rm $year.tar.gz
 done
+
+echo Creating list of all hashes and filenames
+txt_filename="bootstrap-prism-${dataset}.txt"
+ipfs-cluster-ctl pin ls | grep -E "bootstrap-prism-${dataset}-[1-2][0-9]{3}.tar.gz" | awk '{print $3 " " $1}' | sort > $txt_filename
+
+echo Unpinning previous list
+previous_cid=$(ipfs-cluster-ctl pin ls | grep $txt_filename | awk '{print $1}')
+ipfs-cluster-ctl pin rm $previous_cid
+
+echo Pinning the list to the cluster
+cid=$(ipfs-cluster-ctl add --name $txt_filename $txt_filename | awk '{print $2}')
+echo Pinned CID $cid
+
+echo Publishing the list to IPNS
+ipns_key_name="bootstrap-prism-${dataset}"
+ipfs name publish --key="$ipns_key_name" "/ipfs/$cid"
+
+echo Removing list of all hashes and filenames
+rm $txt_filename
