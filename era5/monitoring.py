659b1c6a2ae5d4b4a4411b67d9e2d2ec99aa790a
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2024-12-30 14:27:59 -0500

    hotfix

--- era5/monitoring.py
+++ era5/monitoring.py
@@ -16,6 +16,6 @@ cid_env: str | None = os.getenv("ERA5_TOTAL_PRECIPITATION_CID")
 if cid_env is None:
     print("Error: no CID found through the environment")
 cid_str: str = cid_env  # type: ignore
-hamt = HAMT(store=IPFSStore(), read_only=False, root_node_id=CID.decode(cid_str))
+hamt = HAMT(store=IPFSStore(), read_only=True, root_node_id=CID.decode(cid_str))
 ds = xr.open_zarr(store=hamt)
 print(ds)
