33190e10f0da88a6a1a8258b0c788259a9701861
abidsikder <41392423+abidsikder@users.noreply.github.com>
2025-01-03 12:24:34 -0500

    Create availability-check.py

---
+++ availability-check.py
@@ -0,0 +1,24 @@
+import sys
+
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+
+from py_hamt import HAMT, IPFSStore
+from multiformats import CID
+
+if len(sys.argv) != 2:
+    script_name = sys.argv[0]
+    print(f"Usage: python {script_name} <cid>")
+    sys.exit(1)
+
+cid = CID.decode(sys.argv[1])
+hamt = HAMT(store=IPFSStore(), root_node_id=cid, read_only=True)
+ds = xr.open_zarr(store=hamt)
+print(ds)
+
+random_time = np.random.choice(ds.time)
+ds_slice = ds.sel(time=random_time)
+for var in ds.data_vars:
+    ds_slice[var].plot()  # type: ignore
+    plt.savefig(f"/tmp/plot.png", dpi=300, bbox_inches="tight")
