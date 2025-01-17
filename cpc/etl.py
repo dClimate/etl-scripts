b914776fc22428b6f98bcfc619bc72cb96c85d36
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2025-01-17 16:54:12 -0500

    print to stderr, add documentation

--- cpc/etl.py
+++ cpc/etl.py
@@ -1,9 +1,11 @@
-import datetime
+from datetime import datetime, timezone
 import os
+import sys
 from pathlib import Path
 import subprocess
 
 import click
+import numpy as np
 import xarray as xr
 
 
@@ -57,7 +59,9 @@ def download_year(dataset: str, year: int) -> Path:
 @click.argument("dataset", type=datasets_choice)
 @click.pass_context
 def get_available_timespan(ctx, dataset):
-    click.echo(f"Getting timespan for CPC dataset {dataset}...")
+    """
+    Gets the earliest and latest timestamps for this dataset and prints to stdout. Output looks like f"{earliest} {latest}"
+    """
     start_year: int
     match dataset:
         case "precip-conus":
@@ -67,11 +71,15 @@ def get_available_timespan(ctx, dataset):
         case _:
             raise ValueError(f"Invalid dataset {dataset}")
 
-    current_year = datetime.datetime.now(datetime.UTC).year
-    click.echo(f"Downloading netCDF of year {current_year} to see latest data coverage")
+    current_year = datetime.now(timezone.utc).year
+    print(
+        f"Downloading netCDF of year {current_year} to see latest data coverage",
+        file=sys.stderr,
+    )
     latest_nc = download_year(dataset, current_year)
-    click.echo(
-        f"Downloading netCDF of start year {start_year} to see earliest data coverage"
+    print(
+        f"Downloading netCDF of start year {start_year} to see earliest data coverage",
+        file=sys.stderr,
     )
     earliest_nc = download_year(dataset, start_year)
     ds_latest = xr.open_dataset(latest_nc)
@@ -79,15 +87,15 @@ def get_available_timespan(ctx, dataset):
 
     # Sometimes, right after the start of a new year, you'll get netCDf files just with no data in them
     if len(ds_latest.time) == 0:
-        click.echo(
-            f"Found no data in nc file of year {current_year}, downloading prior year"
+        print(
+            f"Found no data in nc file of year {current_year}, downloading prior year",
+            file=sys.stderr,
         )
         latest_nc = download_year(dataset, current_year - 1)
         ds_latest = xr.open_dataset(latest_nc)
 
-    latest = ds_latest.time[len(ds_latest.time) - 1].values
-    earliest = ds_earliest.time[0].values
-    click.secho("earliest latest", fg="blue")
+    latest: np.datetime64 = ds_latest.time[len(ds_latest.time) - 1].values
+    earliest: np.datetime64 = ds_earliest.time[0].values
     print(f"{earliest} {latest}")
 
 
