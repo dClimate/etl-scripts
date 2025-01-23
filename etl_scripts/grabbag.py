3e2db486615dc514806d9f713ccd1fff3c5cc67a
Abid Sikder <41392423+abidsikder@users.noreply.github.com>
2025-01-23 17:56:26 -0500

    Create grabbag utilities module

---
+++ etl_scripts/grabbag.py
@@ -0,0 +1,10 @@
+"""
+This file houses miscellaneous utilities used by many other scripts. Something should be very universal to be put in here If only used by a couple of files, then stick to copy paste.
+"""
+
+import sys
+
+
+def eprint(arg):
+    """Print one argument to stderr"""
+    print(arg, file=sys.stderr)
